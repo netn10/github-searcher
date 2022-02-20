3c0577542b14480cfedc574a39357fd471b6fc69
netn10 <34625504+netn10@users.noreply.github.com>
2022-02-20 19:11:01 +0200

    App.js updated and backend added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+
+import axios from 'axios';
+import React, { useState } from 'react';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [submit, setSubmit] = useState(false);
+  const [input, setInput] = useState('');
+  const [stars, setStars] = useState('');
+  const [minMax, setMinMax] = useState('');
+  const [date, setDate] = useState('');
+  const [langs, setLangs] = useState('');
+  const [data, setData] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    setSubmit(!submit);
+    console.log(submit);
+
+   axios.post("http://127.0.0.1:5000/search", {
+    input: input,
+    stars: stars,
+    minMax: minMax,
+    date: date,
+    langs: langs,
+  })
+  .then(function (response) {
+    console.log(response.data.items)
+    setData(response.data.items);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+ }
+
+  return(
+    <div className="wrapper">
+      <h1>Github Searcher</h1>
+      {!submit && (<form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Search Input</p>
+          </label>
+          <input name="name" onChange= {(e) => setInput(e.target.value)} value = {input} />
+          <label>
+            <p>By:</p>
+          </label>
+          <label>
+            <p>Stars</p>
+            <select onChange={(e) => setMinMax(e.target.value)} value = {minMax}>
+          <option value="empty"></option>
+          <option value="min">Min</option>
+          <option value="max">Max</option>
+        </select>
+        <input type="number" name="stars" onChange= {(e) => setStars(e.target.value)} value = {stars} />
+
+          </label>
+          <label>
+            <p>Last Update Repository (Date)</p>
+
+          <input name="date" onChange= {(e) => setDate(e.target.value)} value = {date} />
+
+          </label>
+          <label>
+            <p>Programing language/s (Seperate by ,)</p>
+
+          <input name="langs" onChange= {(e) => setLangs(e.target.value)} value = {langs} />
+
+          </label>
+        </fieldset>
+      
+        <button type="submit">Search</button>
+      </form>
+      )}
+      {submit ? <Table data={data} /> : null}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,49 @@
+import React from 'react'
+function Table({data}){
+    if (!(data))
+    {
+        return(
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+    const table=data.map(
+        (d, id)=>{
+            return(
+                <tr key={id}>
+                    <td>{id}</td>
+                    <td>{d.name}</td>
+                    <td>{d.description}</td>
+                    <td>{d.created_at}</td>
+                    <td>{d.open_issues}</td>
+                </tr>
+            )
+        }
+    )
+ 
+    return(
+        <div>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                    <th>id</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Created At</th>
+                    <th>Open Issues</th>
+                    </tr>
+                </thead>
+                <tbody>
+                 
+                    
+                    {table}
+                    
+                </tbody>
+            </table>
+             
+        </div>
+    )
+ }
+ 
+ export default Table;
